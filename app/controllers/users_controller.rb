2fcc40c56329b01fac07fb2e1d6dcab218caff72
Dani Wilson <danirae.powell@gmail.com>
2023-09-05 16:15:54 -0600

    add login logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,36 @@ class UsersController <ApplicationController
     user = User.create(user_params)
     if user.save
       redirect_to user_path(user)
+      session[:user_id] = user.id
     else  
       flash[:error] = user.errors.full_messages.to_sentence
       redirect_to register_path
     end 
   end 
+
+  def login_form
+  end
+
+  def login
+    if !params[:email] || !params[:password] || params[:password] != params[:password_confirmation] 
+      flash[:error] = "Invalid credentials"
+      render :login_form
+    else
+      @user = User.find_by(email: params[:email])
+
+      if @user.nil?
+        flash[:error] = "Invalid credentials"
+        render :login_form
+      elsif @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          flash[:success] = "Welcome, #{@user.name}"
+          redirect_to user_path(@user.id)
+      else 
+        flash[:error] = "Invalid credentials"
+        render :login_form
+      end
+    end
+  end
 
   private 
 
