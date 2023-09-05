b6d960c2f5489fc98012782a1ad65466f99c0ff1
Dani Wilson <danirae.powell@gmail.com>
2023-09-05 14:35:39 -0600

    update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
-class User <ApplicationRecord 
-  validates_presence_of :email, :name 
-  validates_uniqueness_of :email
+class User < ApplicationRecord 
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, allow_blank: false
+  validates_presence_of :password, allow_blank: false
+  validates_presence_of :password_confirmation, allow_blank: false
+
+  has_secure_password
 end 
