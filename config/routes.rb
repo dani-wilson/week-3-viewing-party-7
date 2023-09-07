9999c9b7b17c132c428ced7abbc5b7d078850d09
Dani Wilson <danirae.powell@gmail.com>
2023-09-07 14:01:59 -0600

    refactor routes to include sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie'
+  get '/users/:user_id/movies/:id/viewing_party', to: 'viewing_parties#new'
+  post '/users/:user_id/movies/:id/viewing_party', to: 'viewing_parties#create'
 
   resources :users, only: :show
-  get '/login', to: 'users#login_form'
-  post '/login', to: 'users#login'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
